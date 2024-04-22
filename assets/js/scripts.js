17c4b59de12019ef0a6454be17f0b1e221c43e53
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-04-22 22:04:47 +0330

    commit message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -60,3 +60,16 @@ selectType.forEach((item)=>{
     item.parentElement.parentElement.classList.remove('opacity-30');
   })
 })
+
+// navigation
+
+const list = document.querySelectorAll('.list');
+
+function activeLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((item) =>
+item.addEventListener('mouseover',activeLink));
